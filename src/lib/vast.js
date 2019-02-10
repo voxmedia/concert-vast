8b87c5e16959ab67a4c791c87bb1acc7074e2c52
Brian Anderson <banderson623@gmail.com>
2019-02-10 16:43:53 -0600

    basic remote loading support

--- src/lib/vast.js
+++ src/lib/vast.js
@@ -8,8 +8,8 @@ export default class Vast {
     this.vastUrl = url
     this.vastDocument = null
 
-    if(this.vastXml == undefined &&
-       this.vastUrl == undefined){
+    if (this.vastXml == undefined &&
+       this.vastUrl == undefined) {
       throw TypeError('Vast constructor expects either a xml or an url argument to be passed');
     }
 
@@ -17,15 +17,48 @@ export default class Vast {
       return new vastElement(this)
     })
 
-    this.parse()
+    if (this.vastXml) {
+      this.parse()
+    }
 
-    this.loadedElements.forEach(e => {e.process()})
+    if(this.vastUrl) {
+      this.loadRemoteVast()
+    }
   }
 
   parse() {
     if (!this.vastDocument) {
       const parser = new DOMParser()
       this.vastDocument = parser.parseFromString(this.vastXml, "application/xml")
+      this.processAllElements()
     }
+  }
+
+  loadRemoteVast() {
+    const request = new XMLHttpRequest()
+
+    request.addEventListener("load", (e) => {
+      console.log('load complete', e)
+      this.vastXml = request.response;
+    });
+
+    request.addEventListener("progress", (e) => {
+      console.log('progress', e)
+      const percentComplete = e.loaded / e.total * 100;
+      console.log('progress ... ', percentComplete)
+    });
+
+    request.addEventListener("error", (e) => {
+      console.log('failed', e)
+    });
+
+    request.open("GET", this.vastUrl)
+    console.log('loading remote vast from ', this.vastUrl)
+    console.log(request)
+    request.send('')
+  }
+
+  processAllElements() {
+    this.loadedElements.forEach(e => {e.process()})
   }
 }
