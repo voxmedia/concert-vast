426280c02f3184cf724f712600b52e696cb34cdd
Brian Anderson <banderson623@gmail.com>
2019-02-10 09:25:25 -0600

    tdd javascript development

--- src/lib/vast.js
+++ src/lib/vast.js
@@ -1,11 +1,17 @@
 export default class Vast {
-  constructor({xml}) {
+  constructor({xml, url} = {}) {
     this.vastXml = xml
+    this.vastUrl = url
+
+    if(this.vastXml == undefined &&
+       this.vastUrl == undefined){
+      throw TypeError('Vast constructor expects either a xml or an url argument to be passed');
+    }
   }
 
   parse() {
     const parser = new DOMParser();
-    this.parsedVast =  parser.parseFromString(this.vastXML, "application/xml");
-    return this.parsedVast
+    this.parsedVast =  parser.parseFromString(this.vastXML, "application/xml")
+    return this.parsedVast.toString();
   }
 }
--- test/vast.spec.js
+++ test/vast.spec.js
@@ -1,8 +1,37 @@
-import Vast from '../src/lib/vast';
+import Vast from '../src/lib/vast'
+import * as fs from 'fs'
+import * as process from 'process'
 
-
-describe('Vast', () => {
-  it('Vast can be instantiated', () => {
-    const vast = new Vast({xml:''});
+describe('Basic Vast class functions', () => {
+  it('it can be instantiated with a string', () => {
+    const vast = new Vast({xml: ''})
   });
+
+  it('it can be instantiated with a url', () => {
+    const vast = new Vast({ url: 'https://ad.doubleclick.net/ddm/pfadx/N884.154386.EATER.COM/B21643693.231589573;sz=0x0;ord=[timestamp];dc_lat=;dc_rdid=;tag_for_child_directed_treatment=;tfua=;dcmt=text/xml'})
+  });
+
+  it('raises an error if url or xml is not set', () => {
+    expect(() => {
+      const vast = new Vast()
+    }).toThrow(TypeError)
+  })
+
+  it('handles valid xml and parses it', () => {
+    const xmlString = fs.readFileSync('./test/fixtures/vast.xml')
+    const vast = new Vast({ xml: xmlString })
+    vast.parse();
+  })
+});
+
+describe('Vast Videos', () => {
+  let xmlString
+
+  beforeEach(()=> {
+    xmlString = fs.readFileSync('./test/fixtures/vast.xml')
+  })
+
+  it('can return a list of video files', () => {
+
+  })
 });
