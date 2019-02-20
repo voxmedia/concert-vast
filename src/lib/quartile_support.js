5bc82fa79f16e5197afb408b43cb22ecdb915a7d
Brian Anderson <banderson623@gmail.com>
2019-02-20 18:39:19 -0500

    quartile support library added.

---
+++ src/lib/quartile_support.js
@@ -0,0 +1,53 @@
+const QUARTILES = [
+  [0.0, 'start'],
+  [0.25, 'firstQuartile'],
+  [0.5, 'midpoint'],
+  [0.75, 'thirdQuartile'],
+  [0.99, 'complete'],
+]
+
+export default class QuartileSupport {
+  constructor() {
+    this.seenQuartiles = []
+    this.callbacks = []
+    this.currentTime = 0
+    this.duration = Infinity
+  }
+
+  setDuration(time) {
+    if (time != 0) {
+      this.duration = time
+    }
+    this.checkForQuartileEvent()
+  }
+
+  onQuartileChange(func) {
+    this.callbacks.push(func)
+  }
+
+  setCurrentTime(currentTime) {
+    this.currentTime = currentTime
+    this.checkForQuartileEvent()
+  }
+
+  // private
+
+  checkForQuartileEvent() {
+    const percentComplete = this.currentTime / this.duration
+    const matchingQuartiles = QUARTILES.filter(quartile => {
+      return quartile[0] <= percentComplete
+    })
+
+    matchingQuartiles.forEach(quartile => {
+      const [percent, name] = quartile
+      if (!this.seenQuartiles.includes(percent)) {
+        this.seenQuartiles.push(percent)
+        this.quartileChangeCallback(name)
+      }
+    })
+  }
+
+  quartileChangeCallback(quartileName) {
+    this.callbacks.forEach(fn => fn.call(null, quartileName))
+  }
+}
---
+++ test/quartile_support.spec.js
@@ -0,0 +1,65 @@
+import QuartileSupport from '../src/lib/quartile_support'
+
+describe('Basic Quartile Functionality', () => {
+  it('it has a setDuration function', () => {
+    const qs = new QuartileSupport()
+    expect(typeof qs.setDuration).toBe('function')
+    qs.setDuration(10)
+  })
+
+  it('it has a callback onQuartileChange function', () => {
+    const qs = new QuartileSupport()
+    expect(typeof qs.onQuartileChange).toBe('function')
+  })
+
+  it('it has a setCurrentTime function', () => {
+    const qs = new QuartileSupport()
+    expect(typeof qs.setCurrentTime).toBe('function')
+  })
+})
+
+describe('Callbacks for changes', () => {
+  let qs
+  beforeEach(() => {
+    qs = new QuartileSupport()
+  })
+
+  it('should call a callback after 26 percent complete', () => {
+    let lastQName = null
+    qs.onQuartileChange(quartileName => {
+      lastQName = quartileName
+    })
+
+    qs.setDuration(10)
+    qs.setCurrentTime(4)
+    expect(lastQName).toBe('firstQuartile')
+  })
+
+  it('should call all quartiles if moved forward', () => {
+    let lastQName = null
+    let calls = 0
+    qs.onQuartileChange(quartileName => {
+      calls++
+      lastQName = quartileName
+    })
+
+    qs.setDuration(10)
+    qs.setCurrentTime(10)
+    expect(lastQName).toBe('complete')
+    expect(calls).toBe(5)
+  })
+
+  it('should handle callbacks if duration changes', () => {
+    let lastQName = null
+    qs.onQuartileChange(quartileName => {
+      lastQName = quartileName
+    })
+
+    qs.setDuration(100)
+    qs.setCurrentTime(4)
+    expect(lastQName).toBe('start')
+
+    qs.setDuration(4)
+    expect(lastQName).toBe('complete')
+  })
+})
