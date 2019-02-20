9dd753fd56292f1f3b90de89ed05b67babf2d433
Brian Anderson <banderson623@gmail.com>
2019-02-20 16:12:29 -0500

    weird attempt at this

--- src/lib/applications/video_element.js
+++ src/lib/applications/video_element.js
@@ -1,20 +1,68 @@
+import QuartileSupport from '../qaurtile_support'
+
+const EVENT_MAPPING = {
+  muted: 'mute',
+  ended: 'complete',
+  pause: 'pause',
+}
+
 export default class VideoElement {
   constructor() {
     this.videoElement = null
     this.vast = null
   }
 
-  useVideo(videoElement) {
-    this.videoElement = videoElement
-  }
-
-  useVast(vast) {
+  apply({ vast, videoElement }) {
     this.vast = vast
-  }
+    this.videoElement = videoElement
+    this.quartilesExperienced = []
 
-  apply() {
-    this.setupVideo()
+    this.pauseExistingVideoSources()
+    this.setupVideoListeners()
+    this.loadVastVideo()
     this.setupImpressions()
     this.setupClickHandlers()
+  }
+
+  // private
+
+  pauseExistingVideoSources() {
+    Array.from(this.videoElement.querySelectorAll('source')).forEach(n => {
+      n.setAttribute('vast-delayed-src', n.getAttribute('src'))
+      n.setAttribute('src', null)
+    })
+  }
+
+  setupVideoListeners() {
+    let previousVolume = this.videoElement.volume
+
+    for (const nativeEventName in EVENT_MAPPING) {
+      this.videoElement.addEventListener(nativeEventName, () => {
+        this.vast.addImpressionTrackingImagesFor(EVENT_MAPPING[nativeEventName])
+      })
+    }
+
+    // handle mute support
+    this.videoEvent.addEventListener('volumechange', () => {
+      if (previousVolume == 0 && this.videoElement.volume != 0) {
+        this.vast.addImpressionTrackingImagesFor('unmute')
+      } else if (previousVolume != 0 && this.videoElement.volume == 0) {
+        this.vast.addImpressionTrackingImagesFor('mute')
+      }
+      previousVolume = this.videoElement.volume
+    })
+
+    this.videoEvent.addEventListener('timeupdate', () => {
+      this.checkForQuartileEvent(this.videoElement.currentTime)
+    })
+  }
+
+  loadVastVideo() {
+    // load this vast video
+    const vidSource = document.createElement('source')
+    vidSource.setAttribute('src', bestVideo.url())
+    vidSource.setAttribute('type', bestVideo.mimeType())
+    this.videoElement.appendChild(vidSource)
+    this.videoElement.load()
   }
 }
